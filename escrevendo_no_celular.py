2720fd4dcf31405b65e38a0e73c41d169de28c4b
Hugo Doria <hugo@popcode.com.br>
2013-05-16 20:57:43 -0300

    Testando para passar B - by hdoria - SO SE ViVE uMa VeZ

--- escrevendo_no_celular.py
+++ escrevendo_no_celular.py
@@ -5,10 +5,15 @@ def escrevendo_no_celular(word):
     caracteres = {"ABC": "2", "DEF": "3", "GHI": "4", "JKL": "5", "MNO" : "6", "PQRS": "7", "TUV": "8", "WXYZ": "9", " ": "0"}
 
     output = ""
+    antiga_chave = ''
     for char in word:
         for chave, valor in caracteres.items():
             if char in chave:
+                if antiga_chave == chave:
+                    output += '_'
+
                 output += valor * (chave.find(char) + 1)
+                antiga_chave = chave
 
     return output
 
@@ -27,6 +32,16 @@ class escrevendo_no_celular_test(unittest.TestCase):
 
     def test_B(self):
         self.assertEquals(escrevendo_no_celular("B"), "22")
+
+    def test_AB(self):
+        self.assertEquals(escrevendo_no_celular("AB"), "2_22")
+
+    def test_ABCA(self):
+        self.assertEquals(escrevendo_no_celular("ABCA"), "2_22_222_2")
+
+
+    def test_VIDALOUCA(self):
+        self.assertEquals(escrevendo_no_celular("SEMPRE ACESSO O DOJOPUZZLES"), "77773367_7773302_222337777_777766606660366656667889999_9999555337777")
 
 if __name__ == '__main__':
     unittest.main()
